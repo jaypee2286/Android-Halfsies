2b82b09993e5cfd0e286c54e349bafcd5616fcfe
John Paul Canlas <jaypee2286@gmail.com>
2014-04-01 20:48:31 -0700

    first working version
    tuesday april 1, 2014

--- src/jcanlas/projects/myfirstapp/Diner.java
+++ src/jcanlas/projects/myfirstapp/Diner.java
@@ -2,12 +2,18 @@ package jcanlas.projects.myfirstapp;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.math.BigDecimal;
 
 public class Diner extends MainPage {
 
 	private String name;
-	private List<Integer> itemCosts = new ArrayList<Integer>();
+	private List<Double> itemCosts = new ArrayList<Double>();
 	private List<String> itemNames = new ArrayList<String>();
+	private double total = 0;
+	private double tax = 0;
+	private double tip = 0;
+	private double subtotal = 0;
+	private double grandtotal = 0;
 	
 	public Diner(){
 		initialize();
@@ -17,6 +23,7 @@ public class Diner extends MainPage {
 		setName("Guest");
 	}
 	
+	// get and set for NAME
 	public void setName(String nameIn){
 		name = nameIn;
 	}
@@ -25,21 +32,104 @@ public class Diner extends MainPage {
 		return name;
 	}
 	
+	// get and set for ITEM NAMES
 	public String getItemName(int itemNameIndex){
 		return itemNames.get(itemNameIndex);
 	}
-	public int getItemCost(int itemCostIndex){
+	public Double getItemCost(int itemCostIndex){
 		return itemCosts.get(itemCostIndex);
 	}
 	
-	public void setItem(String itemName, int itemCost){
+	// ADD an item to arrays
+	public void addItem(String itemName, String itemCost){
 		itemNames.add(itemName);
-		itemCosts.add(itemCost);
+		itemCosts.add(Double.parseDouble(itemCost));
 	}
 	
-	public void removeItem(int index){
-		itemCosts.remove(index);
-		itemNames.remove(index);
+	// REMOVE an item from arrays
+	public void removeItem(String name, double cost){
+		for (int i = 0; i < itemCosts.size(); i++){
+			if (itemCosts.get(i)==cost && itemNames.get(i)==name){
+				itemCosts.remove(i);
+				itemNames.remove(i);
+				break;
+			}
+		}
+	}
+	
+	public void clearList(){
+		itemCosts.clear();
+		itemNames.clear();
+	}
+	
+	public void setTotal(double a){
+		total = a;
+	}
+	
+	// calculate cost subtotal
+	public double calculateSubtotal(){
+		setTotal(0);
+		for (int i = 0; i < itemCosts.size(); i++){
+			total += itemCosts.get(i);
+		}
+		total = total * 100;
+		total = Math.round(total);
+		total = total / 100;
+		return total;
+	}
+	
+	// calculate cost tax
+	public double calculateTax(double tax){
+		setTotal(0);
+		for (int i = 0; i < itemCosts.size(); i++){
+			total += itemCosts.get(i);
+		}
+		total = total * (tax/100);
+		total = total * 100;
+		total = Math.round(total);
+		total = total / 100;
+		return total;
+	}
+
+	// calculate cost tax
+	public double calculateTotal(double tax){
+		setTotal(0);
+		for (int i = 0; i < itemCosts.size(); i++){
+			total += itemCosts.get(i);
+		}
+		total = total + (total * (tax/100));
+		total = total * 100;
+		total = Math.round(total);
+		total = total / 100;
+		return total;
+	}
+	
+	// calculate cost tip
+	public double calculateTip(double tip){
+		setTotal(0);
+		for (int i = 0; i < itemCosts.size(); i++){
+			total += itemCosts.get(i);
+		}
+		total = total * (tip/100);
+		total = total * 100;
+		total = Math.round(total);
+		total = total / 100;
+		return total;
+	}
+	
+	// CALCULATE cost total
+	public double calculateGrandTotal(double tax, double tip){
+		setTotal(0);
+		double a = 0;
+		for (int i = 0; i < itemCosts.size(); i++){
+			total += itemCosts.get(i);
+		}
+		a = total + (total * (tax/100));
+		a = a + (total*(tip/100));
+		a = a * 100;
+		a = Math.round(a);
+		a = a / 100;
+		return a;
 	}
 	
 }
--- src/jcanlas/projects/myfirstapp/MainPage.java
+++ src/jcanlas/projects/myfirstapp/MainPage.java
@@ -1,5 +1,7 @@
 package jcanlas.projects.myfirstapp;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,6 +11,7 @@ import android.app.AlertDialog.Builder;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.InputFilter;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
@@ -26,68 +29,130 @@ public class MainPage extends Activity {
 	private EditText itemCost;
 	private EditText itemName_input;
 	private EditText itemCost_input;
+	private EditText tax;
+	private EditText tip;
 	private Button buttonAdd;
 	private Button buttonCalculate;
+	private Button buttonClear;
 	private LinearLayout container;
-	private int numberOfDiners = 0;
 	private int currentDiner = 0;
 	private int currentItem = 0;
 	public int counter = 0;
 	private List<Integer> itemCosts = new ArrayList<Integer>();
+	private int arraycounter = 0;
+	private int arraycounter2 = 0;
+	private double results;
+	private double taxinput;
+	private double tipinput;
+	public DecimalFormat df = new DecimalFormat("0.00");
+	public BigDecimal bd = new BigDecimal(0);
+
+	// tests
+	private String test1;
+	private int test2 = 0;
 
 	final Context context = this;
 	private EditText result;
 	ViewGroup contentArea;
 	private int icon = android.R.drawable.ic_menu_add;
-	
-	private String tempstring1;
-	private String tempstring2;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_page);
 
-		// attach objects to android UI elements
-		itemCost = (EditText) findViewById(R.id.addItems_itemCost);
-		itemName = (EditText) findViewById(R.id.addItems_itemName);
-		itemCost_input = (EditText) findViewById(R.id.prompt_itemCost);
-		itemName_input = (EditText) findViewById(R.id.prompt_itemName);
+		tax = (EditText) findViewById(R.id.input_tax);
+		tip = (EditText) findViewById(R.id.input_tip);
+		tax.setFilters(new InputFilter[] {new DecimalFilter()});
+		tip.setFilters(new InputFilter[] {new DecimalFilter()});
 
 		buttonAdd = (Button) findViewById(R.id.diner_buttonAddItem);
 		buttonCalculate = (Button) findViewById(R.id.diner_buttonCalculate);
+		buttonClear = (Button) findViewById(R.id.diner_buttonClear);
 
 		container = (LinearLayout) findViewById(R.id.container);
 
-		// create a Diner object
-		Diner guest1 = new Diner();
 		
-		// when the ADD button is pressed:
+		// create a Diner object
+		final Diner guest1 = new Diner();
+
+		//
+		// run when ADD button is pressed
 		buttonAdd.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				addItemPrompt();
+				addItemPrompt(guest1);
 			}
 		});
 
-		
 		// when the CALCULATE button is pressed:
 		buttonCalculate.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				calculate();
+				calculate(guest1);
+			}
+		});
+		// when the CLEAR button is pressed:
+		buttonClear.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				container.removeAllViews();
+				guest1.clearList();
 			}
 		});
 
 	}
-	
-	public void calculate() {
-		
-	}
 
-	public void addItemPrompt() {
+	//
+	// "calculate(Diner)"
+	// CALCULATE TOTAL AND DISPLAY IN MESSAGE BOX
+	public void calculate(Diner guest1) {
+		results = guest1.calculateSubtotal();
+		taxinput = Double.parseDouble(tax.getText().toString());
+		tipinput = Double.parseDouble(tip.getText().toString());
+		LayoutInflater resultsPopup = LayoutInflater.from(context);
+		View resultsView = resultsPopup.inflate(R.layout.display_results, null);
+		AlertDialog.Builder resultsPopup_content = new AlertDialog.Builder(
+				context);
+		
+		final TextView results_subtotal = (TextView) resultsView
+				.findViewById(R.id.results_displaySubtotal);
+		final TextView results_tax = (TextView) resultsView
+				.findViewById(R.id.results_displayTax);
+		final TextView results_total = (TextView) resultsView
+				.findViewById(R.id.results_displayTotal);
+		final TextView results_tip = (TextView) resultsView
+				.findViewById(R.id.results_displayTip);
+		final TextView results_grandtotal = (TextView) resultsView
+				.findViewById(R.id.results_displayGrandTotal);
+
+		// format results output
+		results_subtotal.setText(df.format(results));
+		results_total.setText(df.format((guest1.calculateTotal(taxinput))));
+		results_tax.setText(df.format(guest1.calculateTax(taxinput)));
+		results_tip.setText(df.format(guest1.calculateTax(tipinput)));
+		results_grandtotal.setText(df.format(guest1.calculateGrandTotal(taxinput, tipinput)));
+		
+		// set promps.xml to builder
+		resultsPopup_content.setView(resultsView);
+
+		resultsPopup_content.setCancelable(false).setPositiveButton("OK",
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						dialog.dismiss();
+					}
+				});
+		AlertDialog resultsPopup_show = resultsPopup_content.create();
+		resultsPopup_show.show();
+	}
+	
+	//
+	// "addItemPrompt(Diner)"
+	// this creates a popup that prompts user for item data
+	public void addItemPrompt(final Diner guest1) {
 
 		LayoutInflater popup = LayoutInflater.from(context);
 		View promptsView = popup.inflate(R.layout.activity_add_items_prompt,
@@ -98,12 +163,12 @@ public class MainPage extends Activity {
 		// set promps.xml to builder
 		popup_content.setView(promptsView);
 
-		
-		final EditText itemName_input2 = (EditText) promptsView
+		final EditText itemName_input = (EditText) promptsView
 				.findViewById(R.id.prompt_itemName);
-		final EditText itemCost_input2 = (EditText) promptsView
+		final EditText itemCost_input = (EditText) promptsView
 				.findViewById(R.id.prompt_itemCost);
-
+		itemCost_input.setFilters(new InputFilter[] {new DecimalFilter()});
+		
 		// set dialog message
 		popup_content
 				.setCancelable(false)
@@ -111,32 +176,38 @@ public class MainPage extends Activity {
 					public void onClick(DialogInterface dialog, int id) {
 						// get user input and set it to result
 						// edit text
-						//addLine();
-						
-						
-						
+						// addLine();
 
 						LayoutInflater layoutInflater = (LayoutInflater) getBaseContext()
-								.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-						final View addView = layoutInflater.inflate(R.layout.add_items, null);
-						TextView addItems_itemName = (TextView) addView.findViewById(R.id.addItems_itemName);
-						TextView addItems_itemCost = (TextView) addView.findViewById(R.id.addItems_itemCost);
-						addItems_itemName.setText(itemName_input2.getText());
-						addItems_itemCost.setText(itemCost_input2.getText());
-						Button buttonRemove = (Button) addView.findViewById(R.id.remove);
+								.getSystemService(
+										Context.LAYOUT_INFLATER_SERVICE);
+						final View addView = layoutInflater.inflate(
+								R.layout.add_items, null);
+						final TextView addItems_itemName = (TextView) addView
+								.findViewById(R.id.addItems_itemName);
+						final TextView addItems_itemCost = (TextView) addView
+								.findViewById(R.id.addItems_itemCost);
+						addItems_itemName.setText(itemName_input.getText());
+						addItems_itemCost.setText(itemCost_input.getText());
+						Button buttonRemove = (Button) addView
+								.findViewById(R.id.remove);
+						guest1.addItem(addItems_itemName.getText().toString(),
+								addItems_itemCost.getText().toString());
 						buttonRemove.setOnClickListener(new OnClickListener() {
 
 							@Override
 							public void onClick(View v) {
-								((LinearLayout) addView.getParent()).removeView(addView);
-								//container.removeAllViews();
+								((LinearLayout) addView.getParent())
+										.removeView(addView);
+								guest1.removeItem(addItems_itemName.getText()
+										.toString(), Double
+										.parseDouble(addItems_itemCost
+												.getText().toString()));
+								// container.removeAllViews();
 							}
 						});
 						container.addView(addView);
-						
-						
-						
-						
+
 					}
 				})
 				.setNegativeButton("Cancel",
@@ -145,35 +216,10 @@ public class MainPage extends Activity {
 								dialog.cancel();
 							}
 						});
-		
+
 		AlertDialog popup_show = popup_content.create();
 		popup_show.show();
 
-	}
-	
-	public void addLine(){
-		
-		LayoutInflater layoutInflater = (LayoutInflater) getBaseContext()
-				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		final View addView = layoutInflater.inflate(R.layout.add_items, null);
-		TextView addItems_itemName = (TextView) addView.findViewById(R.id.addItems_itemName);
-		TextView addItems_itemCost = (TextView) addView.findViewById(R.id.addItems_itemCost);
-		
-		// textOut.setText(Integer.toString(counter));
-		counter++;
-		// textOut.setText((itemCosts.get(0)).toString());
-		Button buttonRemove = (Button) addView.findViewById(R.id.remove);
-		//contentArea.add
-		buttonRemove.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				((LinearLayout) addView.getParent()).removeView(addView);
-				//container.removeAllViews();
-			}
-		});
-		container.addView(addView);
-		
 	}
 
 	@Override
